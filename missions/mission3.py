f88ba972f35d256c767b1f7b7e4cc0eccf8818eb
UysalEfeYuce <uysalefeyuce55@hotmail.com>
2025-05-06 16:05:21 +0300

    1-Homework
    Tasks for Acunmedya academy

---
+++ missions/mission3.py
@@ -0,0 +1,4 @@
+
+for i in range(1,101):#1 ile 100 arasında i değerini gezdiriyoruz
+    if(i%2==0):#Modu 2 ye bölünüp 0'a eşit olanlar çift olacağı için böyle bir işlem yapıyoruz
+        print(i,"\t")
