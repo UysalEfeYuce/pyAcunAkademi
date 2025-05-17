0e82be27256efc3d6877485cc770b57508ae8ad4
UysalEfeYuce <uysalefeyuce55@hotmail.com>
2025-05-17 13:09:17 +0300

    Update mission.py

--- missions/mission 2/mission.py
+++ missions/mission 2/mission.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
 data = pd.read_csv("trainmedya.csv")
+print("Veri Setinin ilk 5 satırı:")
+print(data.head())
 print("İstatistiksel Özet:")
 print(data.describe())
 print("Veri Seti Bilgisi:")
 print(data.info())
 print("Eksik değerlerin Sayısı:")
 print(data.isnull().sum())
-data['Embarked'] = data['Embarked'].fillna(data['Embarked'].mode()[0])#2 adet eksik değer vardı onları modu ile doldurduk
-data['Cabin'] = data['Cabin'].fillna(data['Cabin'].mode()[0])#687 adet eksik değer vardı onları modu ile doldurduk
-data['Age'] = data['Age'].fillna(data['Age'].mean()) #177 adet eksik değer vardı onları ortalaması ile doldurduk
+data['Embarked'] = data['Embarked'].fillna(data['Embarked'].mode()[0])#2 adet eksik değer vardı onları modu ile doldurdum
+data['Cabin'] = data['Cabin'].fillna(data['Cabin'].mode()[0])#687 adet eksik değer vardı onları modu ile doldurdum
+data['Age'] = data['Age'].fillna(data['Age'].mean()) #177 adet eksik değer vardı onları ortalaması ile doldurdum
 print("Boşluklar başarıyla dolduruldu")
 print(data.isnull().sum())
